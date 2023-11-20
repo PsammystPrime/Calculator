cd32217bb81507612a42fb65f6dd1cf8522e60f9
Samuel Njuguna <samuelprime384@gmail.com>
2023-11-20 10:26:56 +0300

    Update Javascript.js
    Rendered buttons to be clickable and output displayed.

--- Javascript.js
+++ Javascript.js
@@ -1,1 +1,16 @@
+const buttons = document.querySelectorAll('button')
+const screen = document.querySelector('.input')
+
+//add event listers for all buttons
+buttons.forEach(button => {
+    button.addEventListener('click', function (e) {
+      const value = e.target.innerHTML  
+      screen.innerText  +=value 
+     
+      if (value === 'C') {
+        screen.innerText = ''
+      }
+
+    })       
+});
 
