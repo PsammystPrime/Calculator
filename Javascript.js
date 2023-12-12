bd69b426e4cf2979b8f179b74c8e9b8cf41d2827
PsammystPrime <127736051+PsammystPrime@users.noreply.github.com>
2023-12-12 20:01:12 +0300

    update calculation functions

--- Javascript.js
+++ Javascript.js
@@ -1,31 +1,69 @@
-const buttons = document.querySelectorAll('button')
-const previousScreen = document.querySelector('.input')
-const operator= document.querySelector('.operator')
-const  currentScreen= document.querySelector('.inputB')
+const display =document.querySelector('.input');
+const buttons =document.querySelectorAll('.number');
+const decimal = document.querySelector('.decimal')
+const operator = document.querySelectorAll('.operator')
+const equal = document.querySelector('.equals')
+const clear = document.querySelector('.clear')
+const one = document.querySelector('.inputB')
+const two = document.querySelector('.inputC')
+const array= []
 
-//add event listers for all buttons
+
+//add event listeners for each button
 buttons.forEach(button => {
-    button.addEventListener('click', function (e) {
-      const value = e.target.innerHTML  
-      
-     
-      if (value === 'C') {
-        previousScreen.textContent = '';
-      }else if (value === 'D') {
-        previousScreen.textContent = previousScreen.textContent.slice(0, -1);
-      }else if (value === '.') {
-        if (!previousScreen.textContent.includes('.')) {
-          previousScreen.textContent  +=value; 
-        };
-      }else if (value==='*'||value==='/'||value==='+'||value==='-') {
-        currentScreen.textContent = previousScreen.textContent + value
-        previousScreen.textContent=''
-      
-      }else {
-         previousScreen.textContent  +=value;
-      }
-      
-
-    })       
+ button.addEventListener('click', function(e) {
+  let value = e.target.innerHTML
+  display.textContent += value
+  
+array.push(value.toString(''))
+const newArr=array.toString()
+console.log(newArr)
+console.log(array)
+ })
 });
 
+
+operator.forEach(operator =>{
+  operator.addEventListener('click', (e)=>{
+  let value = e.target.innerHTML
+two.textContent = display.textContent
+display.textContent = ''
+one.textContent = value
+  });
+});
+
+equal.addEventListener('click', ()=>{
+  let a = Number(two.textContent)
+  let b =one.textContent
+  let c = Number(display.textContent)
+  if (b==='*') {function multiply(){
+display.textContent= a*c
+  }
+    multiply()
+  }
+  if (b==='/') {
+    return display.textContent= a/c
+  }
+  if (b==='+') {
+  return display.textContent= a+c
+
+  }
+  if (b==='-') {
+    return display.textContent= a-c
+  }  
+});
+
+clear.addEventListener('click', ()=>{
+  display.textContent = ''
+  one.textContent = ''
+  two.textContent = ''
+})
+
+decimal.addEventListener('click', (e)=>{
+  let value = e.target.innerHTML
+  if (!display.textContent.includes('.')){
+
+    display.textContent +=value
+  
+  }
+})
